218ae4b333cc35dd9713206f15a0194c48c1d720
Mateusz <matisiek11@gmail.com>
2020-02-28 22:59:05 +0100

    Implemented beforeRead hook

--- src/interfaces/deployment.interface.ts
+++ src/interfaces/deployment.interface.ts
@@ -1,11 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
 
+export class HookInstance {
+  handler: any;
+  projectId: string;
+  event: string;
+  collection: string;
+  deploymentId: string;
+
+
+  constructor(handler: any, projectId: string, event: string, collection: string, deploymentId: string) {
+    this.handler = handler;
+    this.projectId = projectId;
+    this.event = event;
+    this.collection = collection;
+    this.deploymentId = deploymentId;
+  }
+}
+
 export class DeploymentInstance {
   handler: any;
   projectId: string;
   name: string;
   type: string;
+  deploymentId: string;
 }
 
 export class DeploymentRequest {
